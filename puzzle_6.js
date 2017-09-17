3f73350e5a1cb2c440c930f06c890314331c442c
yyyyyuan <31830607+yyyyyuan@users.noreply.github.com>
2017-09-17 14:51:56 -0700

    Create puzzle_6.js

---
+++ puzzle_6.js
@@ -0,0 +1,19 @@
+right();
+getColor();
+if (getColor()=="red"){
+up();
+up();
+up();
+var direction=up;
+}else{
+down();
+down();
+down();
+var dirction=down}
+getColor();
+if (getColor()=="red"){
+left();
+direction();}
+else{
+right();
+direction()}
